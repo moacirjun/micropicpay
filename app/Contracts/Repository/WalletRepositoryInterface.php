7adacb0bd86f7aed13b528d663d72e81038e51fe
Moacir Lima Jr <moacirjunior.eletro@gmail.com>
2020-03-08 19:28:17 -0300

    findOneByUser method added in Wallet repository

--- app/Contracts/Repository/WalletRepositoryInterface.php
+++ app/Contracts/Repository/WalletRepositoryInterface.php
@@ -2,9 +2,14 @@
 
 namespace App\Contracts\Repository;
 
+use App\Entity\User;
 use Prettus\Repository\Contracts\RepositoryInterface;
 
 interface WalletRepositoryInterface extends RepositoryInterface
 {
-
+    /**
+     * @param User|integer $user
+     * @return mixed
+     */
+    public function findOneByUser($user);
 }
--- app/Repository/WalletRepository.php
+++ app/Repository/WalletRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Contracts\Repository\WalletRepositoryInterface;
+use App\Entity\User;
 use Prettus\Repository\Eloquent\BaseRepository;
 
 class WalletRepository extends BaseRepository implements WalletRepositoryInterface
@@ -13,5 +14,21 @@ class WalletRepository extends BaseRepository implements WalletRepositoryInterfa
     public function model()
     {
         return 'App\Entity\Wallet';
+    }
+
+    /**
+     * @param User|integer $user
+     * @return mixed
+     */
+    public function findOneByUser($user)
+    {
+        $userID = $user instanceof User ? $user->id : $user;
+        $collection = $this->findWhere(['user_id' => $userID]);
+
+        if (sizeof($collection) === 0) {
+            return null;
+        }
+
+        return $collection->first();
     }
 }
